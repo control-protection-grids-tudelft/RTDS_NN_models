519e13365795ced262650f4b291558adefbf4378
Aleksandra Lekic <alekic@tudelft.nl>
2024-03-28 23:15:50 +0100

    Random files

---
+++ NNLib/backprops1_types.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: backprops1_types.h
+ *
+ * Code generated for Simulink model 'backprops1'.
+ *
+ * Model version                  : 1.4
+ * Simulink Coder version         : 9.9 (R2023a) 19-Nov-2022
+ * C/C++ source code generated on : Fri Feb  9 04:02:23 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_backprops1_types_h_
+#define RTW_HEADER_backprops1_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_backprops1_T RT_MODEL_backprops1_T;
+
+#endif                                 /* RTW_HEADER_backprops1_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
