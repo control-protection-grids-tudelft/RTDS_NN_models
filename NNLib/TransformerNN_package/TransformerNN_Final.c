edb30405f6f96a6207f3bde2e42da20ebc0b5e1b
HVDC/AC Control <131252855+control-protection-grids-tudelft@users.noreply.github.com>
2026-06-20 09:57:00 +0200

    Transformer NN
    Not tested.

---
+++ NNLib/TransformerNN_package/TransformerNN_Final.c
@@ -0,0 +1,9 @@
+VERSION:
+3.001
+
+#include "TransformerNN_Final.h"
+
+STATIC:
+double W1[20] = {0.12, -0.08, 0.23, -0.05, 0.07, -0.11, 0.18, 0.03, -0.14, 0.09, 0.02, -0.06, 0.04, 0.15, -0.02, 0.11, -0.07, 0.05, 0.01, -0.03};
+...
+ResetLosses(Layers);ResetNodes(Layers);
